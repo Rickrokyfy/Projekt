3def39efb159e9d3d671073478d4d572c5686319
Rickrokyfy <68117552+Rickrokyfy@users.noreply.github.com>
2021-09-02 22:31:15 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,19 +4,20 @@ using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-
+using System.IO;
 
 namespace Csharp_base
 {
     class Program
     {
         
-        public static void Hanteraunderkommentarer(JObject jObject)
+        public static void Hanteraunderkommentarer(JObject jObject, ref int antalkommentarer)
         {
             //Console.WriteLine(jObject.ToString());
             //Hämta underkommentarsfält om möjligt
-            if(jObject.GetValue("replies").ToString()!="")
+            if(jObject.GetValue("replies")!=null)
             {
+                //Försök läsa replies TODO Kan vara uttdaterad och överflödig
                 try
                 {
                     JObject underkomfalt = JObject.Parse(jObject.GetValue("replies").ToString());
@@ -34,7 +35,16 @@ namespace Csharp_base
                         if (tempob.GetValue("data").ToString() != "")
                         {
                             if (JObject.Parse(tempob.GetValue("data").ToString()).GetValue("body") != null)
+                            {
+
                                 Console.WriteLine(JObject.Parse(tempob.GetValue("data").ToString()).GetValue("body").ToString());
+                                antalkommentarer++;
+                            }
+                            //Om den har underkommentarer hanteras dessa
+                            if(JObject.Parse(tempob.GetValue("data").ToString()).GetValue("replies").ToString() != "")
+                            {
+                                Hanteraunderkommentarer(JObject.Parse(tempob.GetValue("data").ToString()), ref antalkommentarer);
+                            }
                         }
 
                     }
@@ -43,22 +53,12 @@ namespace Csharp_base
                     System.Environment.Exit(1);
                 }
             }
-            
-
-
-          
-
-
-
-
-
         }
-        static void Main(string[] args)
+
+        public static void process_post(string texttoanalyse, ref int antalkommentarer)
         {
-            Console.WriteLine("Hello World!");
-            string text = System.IO.File.ReadAllText(@"C:\Users\01Ahl\source\repos\Csharp_base\input.txt");
             //Hämta ut objekten
-            JArray asjson = JArray.Parse(text);
+            JArray asjson = JArray.Parse(texttoanalyse);
             //Objekt nr 2 är av intresse
             JToken comments = asjson.Last;
             //Konvertera till Jsonobject
@@ -69,9 +69,9 @@ namespace Csharp_base
             JArray ar_ob = JArray.Parse(dat_ob.GetValue("children").ToString());
             JToken temp;
             JObject tempob, dataob;
-            JArray tempar;
             string commenttext;
             //Läs genom alla kommentarer i kommentarsfältet
+            antalkommentarer += ar_ob.Count;
             for (int i = 0; i < ar_ob.Count; i++)
             {
                 //Hämta det objekt som inehåller informationen om kommentaren
@@ -79,23 +79,47 @@ namespace Csharp_base
                 tempob = JObject.Parse(temp.ToString());
                 //Hämta dataobjektet
                 dataob = JObject.Parse(tempob.GetValue("data").ToString());
-                
-                if(dataob.ToString()!="")
+
+                if (dataob.ToString() != "")
                 {
                     //Hämta kommentarstexten om möjligt
-                    if(dataob.ContainsKey("body"))
+                    if (dataob.ContainsKey("body"))
                     {
 
                         commenttext = dataob.GetValue("body").ToString();
                         Console.WriteLine(commenttext);
-                        //Hämta kommentarsfältet
-                        //tempar = JArray.Parse(dataob.GetValue("children").ToString());
                         //Hämta underkommentarer
-                        Hanteraunderkommentarer(dataob);
+                     
+                        Hanteraunderkommentarer(dataob, ref antalkommentarer);
                     }
                 }
             }
 
+        }
+        public static string Get_uri(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+        static void Main(string[] args)
+        {
+            //TODO HÄMTA 
+            //Hämta ngt från reddit genom att ange uri
+            //TODO VISSA SAKER KANSKE KRÄVER API NYCKEL
+            Console.WriteLine("Ange uri: ");
+            string uri = Console.ReadLine();
+            string result = Get_uri(uri);
+            //TODO HÄMTA ADRESSER TILL ETT ANTAL POSTER INNOM ETT GIVET TIDSINTERVALL GIVET SUBREDDIT
+            int antalkommentarer = 0;
+            process_post(result, ref antalkommentarer);
+            Console.WriteLine(antalkommentarer);
 
         }
     }
