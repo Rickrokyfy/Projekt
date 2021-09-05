30d81444a603fc51da2c1f164a54aaa9d4fafc6b
Rickrokyfy <68117552+Rickrokyfy@users.noreply.github.com>
2021-09-05 02:04:20 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,16 +8,68 @@ using System.IO;
 
 namespace Csharp_base
 {
+    
     class Program
     {
-        
-        public static void Hanteraunderkommentarer(JObject jObject, ref int antalkommentarer)
+        public class Database
         {
-            //Console.WriteLine(jObject.ToString());
+            //Fält och antalet av de vanligaste orden
+            public List<string> common = new List<string>();
+            //Fält med possitiva nyckelord
+            public List<string> positiva = new List<string>();
+            //Fält med negativa nyckelord
+            public List<string> negativa = new List<string>();
+            //Överlagrad konstruktor
+            public Database(string comword_file, string pos_word_file, string neg_word_file) 
+            {
+                //Läs och lägg till common words tills den filen är slut
+                StreamReader comstream = new StreamReader(comword_file);
+                string temp;
+                while((temp = comstream.ReadLine())!=null && temp!="")
+                {
+                    common.Add(temp);
+                }
+                //Läs och lägg till possitiva ord
+                StreamReader posstream = new StreamReader(pos_word_file);
+                while((temp = posstream.ReadLine())!=null && temp != "")
+                {
+                    positiva.Append(temp);
+                }
+                //Läs och lägg till negativa ord
+                StreamReader negstream = new StreamReader(neg_word_file);
+                while((temp= negstream.ReadLine())!=null && temp != "")
+                {
+                    negativa.Append(temp);
+                }
+            }
+        }
+        
+        public static void textanalys(string texten, ref Database databas)
+        {
+            //Konvertera texten till lower case
+            string lower_text = texten.ToLower();
+            //Om texten inehåller ett nyckelord relaterat till bitcoin och inte uppger att den är en bot undersöks den
+            if ((lower_text.Contains("btc") || lower_text.Contains("bitcoin") || lower_text.Contains("xbt"))&&!lower_text.Contains("i am a bot") &&!lower_text.Contains("i'm a bot"))
+            {
+                //Sortera bort de vanligaste orden i engelska
+                for(int i=0; i<databas.common.Count(); i++)
+                {
+                    lower_text =lower_text.Replace(" "+databas.common[i]+" ", " ");
+                    lower_text = lower_text.Replace(" " + databas.common[i] + ".", " ");
+                    lower_text = lower_text.Replace("." + databas.common[i] + " ", " ");
+                }
+                //Skriv ut kommentarstexten
+                Console.WriteLine(lower_text);
+            }
+
+
+        }
+        public static void Hanteraunderkommentarer(JObject jObject, ref int antalkommentarer, ref Database databas)
+        {
             //Hämta underkommentarsfält om möjligt
             if(jObject.GetValue("replies")!=null)
             {
-                //Försök läsa replies TODO Kan vara uttdaterad och överflödig
+                //Försök läsa replies 
                 if (jObject.GetValue("replies").ToString() != "")
                 {
                     JObject underkomfalt = JObject.Parse(jObject.GetValue("replies").ToString());
@@ -35,10 +87,14 @@ namespace Csharp_base
                         //OM underkommentarers data och namn kan brytas ut hämtas dessa
                         if (tempob.GetValue("data").ToString() != "")
                         {
+                            //Hämta kommentarstext
                             if (JObject.Parse(tempob.GetValue("data").ToString()).GetValue("body") != null)
                             {
-                                 commenttext = JObject.Parse(tempob.GetValue("data").ToString()).GetValue("body").ToString();
-                                Console.WriteLine(commenttext);
+
+                                commenttext = JObject.Parse(tempob.GetValue("data").ToString()).GetValue("body").ToString();
+                                //Analysera kommentarstexten
+                                textanalys(commenttext, ref databas);
+                                //Console.WriteLine(commenttext);
                                 antalkommentarer++;
                             }
                             //Om den har underkommentarer hanteras dessa
@@ -46,9 +102,13 @@ namespace Csharp_base
                             {
                                 if (JObject.Parse(tempob.GetValue("data").ToString()).GetValue("replies").ToString() != "")
                                 {
-                                    Hanteraunderkommentarer(JObject.Parse(tempob.GetValue("data").ToString()), ref antalkommentarer);
+                                    Hanteraunderkommentarer(JObject.Parse(tempob.GetValue("data").ToString()), ref antalkommentarer, ref databas);
                                 }
+                                
+                                //Console.WriteLine(JObject.Parse(tempob.GetValue("data").ToString()).GetValue("score").ToString());
                             }
+
+                            
 
                         }
 
@@ -58,12 +118,36 @@ namespace Csharp_base
             }
         }
 
-        public static void process_post(string texttoanalyse, ref int antalkommentarer)
+        public static void process_post(string texttoanalyse, ref int antalkommentarer, ref Database databas)
         {
-           
+            
             //Hämta ut objekten
             JArray asjson = JArray.Parse(texttoanalyse);
-            //Objekt nr 2 är av intresse
+            //Hämta posttext
+
+            //TODO HÄMTA UT OCH ANALYSERA POSTENS TEXT OM POSTEN HAR EN TEXT
+            //POSTENS TEXT HITTAS GENOM: PLATS 1 I ARRAY, DATA, CHILDREN, DATA, SELFTEXT
+            //Hämta info om huvudkommentar
+            JObject postob = JObject.Parse(asjson[0].ToString());
+            JObject post_dat_ob = JObject.Parse(postob.GetValue("data").ToString());
+            JArray post_ar_ob = JArray.Parse(post_dat_ob.GetValue("children").ToString());
+            //Från post ar ob hämtas element 1 och des parameter ob,
+            //Om posten är mer än bara en länk
+            if(post_ar_ob.ToString()!="[]")
+            {
+                JObject finalob = JObject.Parse(JObject.Parse(post_ar_ob[0].ToString()).GetValue("data").ToString());
+                //Om detta objekt inehåller en selftext
+                if (finalob.GetValue("selftext") != null)
+                {
+                    Console.WriteLine("POSTTEXT " + finalob.GetValue("selftext").ToString());
+                    string postbody = finalob.GetValue("selftext").ToString();
+                    textanalys(postbody, ref databas);
+                }
+            }
+            
+            
+            //OBS! DENNA KOD ÄR SKIT PGA HAR MED MASSA EXTRACHECKAR, ÄNDRA BARA STARTVÄRDE FÖR LOOPEN TILL 1 så löser sig nog mycket
+            //Hämta kommentarer
             JToken comments = asjson.Last;
             //Konvertera till Jsonobject
             JObject com_ob = JObject.Parse(comments.ToString());
@@ -71,30 +155,36 @@ namespace Csharp_base
             JObject dat_ob = JObject.Parse(com_ob.GetValue("data").ToString());
             //Hämta ut info i  "children" 
             JArray ar_ob = JArray.Parse(dat_ob.GetValue("children").ToString());
+           
             JToken temp;
             JObject tempob, dataob;
             string commenttext;
-            //Läs genom alla kommentarer i kommentarsfältet
+            //Läs genom alla kommentarer, dvs de arobs som är över 0, i kommentarsfältet
+            //TODO TA BORT EXTRACHECKAR, DE FANNS BARA FÖR ATT FÖRUT HANTERADES INTE POSTEN SEPARAT VILKET GJORDE ATT SAKER KUKA UR
             antalkommentarer += ar_ob.Count;
             for (int i = 0; i < ar_ob.Count; i++)
             {
                 //Hämta det objekt som inehåller informationen om kommentaren
                 temp = ar_ob[i];
                 tempob = JObject.Parse(temp.ToString());
+                
                 //Hämta dataobjektet
                 dataob = JObject.Parse(tempob.GetValue("data").ToString());
-
                 if (dataob.ToString() != "")
                 {
+                    //Skriv score
+                    //Console.WriteLine(dataob.GetValue("score").ToString());
                     //Hämta kommentarstexten om möjligt
                     if (dataob.ContainsKey("body"))
                     {
 
                         commenttext = dataob.GetValue("body").ToString();
-                        Console.WriteLine(commenttext);
+                        //Console.WriteLine(dataob.GetValue("score").ToString());
+                        textanalys(dataob.GetValue("body").ToString(), ref databas);
+                        //Console.WriteLine(commenttext);
                         //Hämta underkommentarer
                      
-                        Hanteraunderkommentarer(dataob, ref antalkommentarer);
+                        Hanteraunderkommentarer(dataob, ref antalkommentarer, ref databas);
                     }
                 }
             }
@@ -113,37 +203,60 @@ namespace Csharp_base
             }
         }
 
-        public static void api_get_posts(string uri)
+        public static void api_get_posts(string uri, string reddit,  string sluttid, ref int total_processed_texts, ref Database databas)
         {
-            //Hämta ut dataobjektet
-            string mellanhand = JObject.Parse(uri).GetValue("data").ToString();
-            Console.WriteLine(mellanhand);
-            JArray jArray = JArray.Parse(mellanhand);
-            //Gå igenom alla object i arrayen
-            string urltemp;
-            JObject tempob;
-            int total_processed_texts = jArray.Count();
-            //Gå igenom alla objekt i arrayen
-            for (int i=0; i<jArray.Count(); i++)
+            //Om dataobjektet inte är tomt
+            if (JObject.Parse(uri).GetValue("data").ToString()!="[]")
             {
-                //Hämta ut url koden
-                tempob = JObject.Parse(jArray[i].ToString());
-                urltemp = tempob.GetValue("url").ToString();
-                //Undersök denna urls post
-                process_post(Get_uri(urltemp+="/.json"), ref total_processed_texts);
+                //Hämta ut dataobjektet
+                string mellanhand = JObject.Parse(uri).GetValue("data").ToString();
+                JArray jArray = JArray.Parse(mellanhand);
+                //Öka antal hanterade texter
+                total_processed_texts += jArray.Count();
+                //Gå igenom alla object i arrayen
+                string urltemp;
+                JObject tempob;
+                //Gå igenom alla objekt i arrayen
+                for (int i = 0; i < jArray.Count(); i++)
+                {
+                    //Hämta ut url koden
+                    tempob = JObject.Parse(jArray[i].ToString());
+                    urltemp = tempob.GetValue("full_link").ToString();
+                    Console.WriteLine(urltemp);
+                    //Undersök denna urls post
+                    process_post(Get_uri(urltemp += "/.json"), ref total_processed_texts, ref databas);
+                }
+                //Hämta ny tid att undersöka, sista gammla tid +1
+                int newtime = int.Parse(JObject.Parse(jArray[jArray.Count() - 1].ToString()).GetValue("created_utc").ToString())+1;
+                string newuri = "https://api.pushshift.io/reddit/search/submission/?subreddit=" + reddit + "&sort=desc&sort_type=created_utc&after=" + newtime + "&before=" + sluttid + "&size=1000";
+                //Gör sökning men med ny tid
+                api_get_posts(uri, reddit,  sluttid, ref total_processed_texts, ref databas);
             }
-            //Skriv ut antal hanterade texter
-            Console.WriteLine(total_processed_texts);
+            //annars
+            else
+            {
+                //Skriv ut antal hanterade texter
+                Console.WriteLine(total_processed_texts);
+            }
+
         }
         static void Main(string[] args)
         {
-            //Hämta sidan som ska undersökas
-            Console.WriteLine("Ange sida");
-            string soksida = Console.ReadLine();
+            //Hämta subreddit
+            Console.WriteLine("Vilken subreddit ska undersökas?");
+            string reddit = Console.ReadLine();
+            Console.WriteLine("Ange starttid för undersökning i unix tid");
+            string starttid = Console.ReadLine();
+            Console.WriteLine("Ange sluttid för undersökning i unix tid");
+            string sluttid = Console.ReadLine();
+            string sokning = "https://api.pushshift.io/reddit/search/submission/?subreddit=" +reddit+"&sort=desc&sort_type=created_utc&after="+ starttid +"&before="+ sluttid+"&size=1000";
             //Hämta sidans text
-            string output = Get_uri(soksida);
-            //Gå igenom och hämta poster utifrån texten
-            api_get_posts(output);
+            string output = Get_uri(sokning);
+            //Skapa databasen
+            Database databas = new Database(@"C:\Users\01Ahl\Desktop\C#_projekt_mapp\com.txt", @"C:\Users\01Ahl\Desktop\C#_projekt_mapp\neg.txt", @"C:\Users\01Ahl\Desktop\C#_projekt_mapp\pos.txt");
+            //Gå igenom och hämta poster utifrån texten, dessa analyseras sedan mha databasen
+            int hanteradetexter = 0;
+            api_get_posts(output, reddit, sluttid, ref hanteradetexter, ref databas);
 
         }
     }
